148b963da7f3c5e607d3844bfe7e4aa9985fe6ef
Larry Jones <larry.jones@reveal-energy.com>
2022-08-03 04:47:50 -0500

    Compile using tsconfig.json

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,10 @@
 function startGame() {
     // start the game
     var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting game...';
+    // when `strict: true` is selected, compiler reports that `messagesElement` may be `null`. Since I "know" this
+    // element exists on the page, I will use the "null assertion operator", `!`, to address the error message.
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting game...';
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+// I will apply a similar solution to possible `null` values here.
+document.getElementById('startGame')!.addEventListener('click', startGame);
